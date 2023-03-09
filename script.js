8e3a987507909301be3febabc3061bbd200f57af
Henrique Taffo <henrique.taffo@capgemini.com>
2023-03-09 10:22:39 -0300

    JavaScript Countdown Variables

---
+++ script.js
@@ -0,0 +1,15 @@
+let countDown;
+let interval;
+const endDate = document.querySelector('input[name="endDate"]');
+const clock = document.querySelector('relogio');
+const daysSpan = clock.querySelector('.dias');
+const hoursSpan = clock.querySelector('.horas');
+const minutesSpan = clock.querySelector('.minutos');
+const secondsSpan = clock.querySelector('.segundos');
+
+
+
+
+
+
+// console.log(endDate.value);
