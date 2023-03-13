7a50a49403c7f8ecc311855be1e7fcd7318a3d3c
Henrique Taffo <henrique.taffo@capgemini.com>
2023-03-13 11:49:19 -0300

    JavaScript Date Get Time Remaining

--- script.js
+++ script.js
@@ -25,7 +25,12 @@ function startClock(endTime){
 }
 
 function timeRemaining(endTime){
-    return 'checked';
+    let t = Date.parse(endTime) - Date.parse(new Date());
+    let seconds = Math.floor((t/1000)%60);
+    let minutes = Math.floor((t/1000/60)%60);
+    let hours =  Math.floor((t/(1000*60*60))%24);
+    let days = Math.floor((t/(1000*60*60*24)));
+    return {'total':t,'days':days,'hours':hours,'minutes':minutes,'seconds':seconds};
 }
 
 
